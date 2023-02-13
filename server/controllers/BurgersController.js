18439b32394e33f25d6d94fb4d80d7a4d1af274d
Ryan Wittman <OfficiallyRy@outlook.com>
2023-02-13 16:55:37 -0700

    Done with all API side of project and stuff this aint a real site bro

--- server/controllers/BurgersController.js
+++ server/controllers/BurgersController.js
@@ -1,6 +1,7 @@
 import { fakeDb } from "../db/FakeDb.js";
 import { burgersService } from "../services/BurgersService.js";
 import BaseController from "../utils/BaseController.js";
+import { logger } from "../utils/Logger.js";
 
 export class BurgersController extends BaseController{
 
@@ -9,6 +10,9 @@ export class BurgersController extends BaseController{
     this.router
     .get('', this.getBurgers)
     .get('/:id', this.getBurgerByID)
+    .post('', this.createBurger)
+    .put('/:id', this.editBurger)
+    .delete('/:id', this.deleteBurger)
   }
 
   getBurgers(req, res, next){
@@ -25,6 +29,37 @@ export class BurgersController extends BaseController{
       let burgerID = req.params.id
       const burger = burgersService.getBurgerByID(burgerID)
       res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  createBurger(req, res, next){
+    try {
+      let rawBurgerData = req.body
+      let newBurger = burgersService.createBurger(rawBurgerData)
+      res.send(newBurger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  editBurger(req, res, next){
+    try {
+      let burgerData = req.body
+      let burgerID = req.params.id
+      let burgerToEdit = burgersService.editBurger(burgerID, burgerData)
+      res.send(burgerToEdit)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  deleteBurger(req, res, next){
+    try {
+      let burgerID = req.params.id
+      let burgerToDelete = burgersService.deleteBurger(burgerID)
+      res.send(burgerToDelete)
     } catch (error) {
       next(error)
     }
--- server/services/BurgersService.js
+++ server/services/BurgersService.js
@@ -1,7 +1,39 @@
 import { fakeDb } from "../db/FakeDb.js"
+import { Burger } from "../models/Burger.js"
 import { BadRequest } from "../utils/Errors.js"
 
 class BurgersService{
+  deleteBurger(burgerID) {
+  let burgerToDelete = fakeDb.burgers.findIndex(b => b.id == burgerID)
+  if(burgerToDelete == -1){
+    throw new BadRequest('Bad Burger ID! Yuck!')
+  }
+  const removed = fakeDb.burgers.splice(burgerToDelete, 1)
+  return removed[0]
+  }
+  editBurger(burgerID, burgerData) {
+    let burgerToEdit = fakeDb.burgers.find(b => b.id == burgerID)
+    if(!burgerToEdit){
+      throw new BadRequest('Bad Burger ID. Yuck!')
+    }
+    if(!burgerData.patties && !burgerData.buns){
+      throw new BadRequest('Invalid Burger! Yuck!')
+    }
+
+    burgerToEdit.buns = burgerData.buns
+    burgerToEdit.patties = burgerData.patties
+    burgerToEdit.toppings = burgerData.toppings
+    return burgerToEdit
+  }
+  createBurger(rawBurgerData) {
+    if(!rawBurgerData.patties && !rawBurgerData.buns){
+      throw new BadRequest('Invalid Burger! Yuck!')
+    }
+
+    rawBurgerData.id = Math.floor(Math.random()) * 1000000000 + 'abc' + Math.floor(Math.random() * 100000)
+
+    fakeDb.burgers.push(new Burger(rawBurgerData))
+  }
   getBurgerByID(burgerID) {
   const burger = fakeDb.burgers.find(b => b.id == burgerID)
   if(!burger){
